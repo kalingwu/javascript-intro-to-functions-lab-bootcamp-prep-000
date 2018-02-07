d2d3c06042a123497a4d839a35c8614a99969e58
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-07 16:02:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you!"
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if ($(string) === "I love you, Gramma."`) {
+  } else if ($(string) === `"I love you, Gramma."`) {
     return "I love you, too."
   }
 }
