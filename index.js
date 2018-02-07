6b3ba3eb256af4322cb23f3dacadcad314dc170a
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-07 16:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function sayHiToGrandma(string) {
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } 
+    else if (string === "I love you, Gramma.") {
   else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
